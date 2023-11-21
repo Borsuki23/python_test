d6ea1273131e4ce53c47835dee78f16431f4e80b
Borsuki23 <arturgnat2009@gmail.com>
2023-11-21 11:42:09 +0200

    dz2 я не розумію які саме тести планує робити моя програма тому прошу вас допомогти мені і пояснити як рробити та виконати це через пайтест адже я його ввімкнув в налаштуваннях по відео ютуб навіть і через нього він не запускається але конвертатор сантиметрів в дюйми я зроби прошу допомоготи.

--- dz2.py
+++ dz2.py
@@ -1,3 +1,12 @@
-
-
-
+def cm_to_inches(length_cm):
+    if length_cm < 0:
+        return -1 * length_cm
+    else:
+        return round(length_cm / 2.54, 2)
+try:
+    length_cm = float(input("Введіть довжину в сантиметрах: "))
+except ValueError:
+    print("Будь ласка, введіть числове значення.")
+else:
+    length_inches = cm_to_inches(length_cm)
+    print(f"{length_cm} см = {length_inches} дюймів")
